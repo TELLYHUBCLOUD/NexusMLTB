675192eca14ddfcd809f84cdc3f5f03a7a6c08f1
mvxyoffcial-create <mvxyoffcial@gmail.com>
2026-03-15 11:53:44 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,12 @@
+# Import all handler modules so decorators register with the Pyrogram client
+from . import start
+from . import info
+from . import admin
+from . import session
+from . import settings
+from . import video
+from . import audio
+from . import document
+from . import url
+from . import mirror
+from . import upload
